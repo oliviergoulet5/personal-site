cb1a4ad53cd20858069b75e355630e03c9a907bd
Olivier Goulet <olivier_goulet@icloud.com>
2022-03-07 22:26:19 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<main>
+	<nav>
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li>Projects</li>
+			<li>Resume</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+</main>
+
+<style>
+	main {
+		font-weight: bold;
+		font-size: 0.875rem;
+		line-height: 1.25rem;
+	}
+
+	nav {
+		padding: 1rem 0;
+	}
+
+	nav ul {
+		display: flex;
+		justify-content: center;
+		list-style-type: none;
+		text-transform: uppercase;
+	}
+
+	nav ul li {
+		margin-left: 2rem;
+	}
+</style>
