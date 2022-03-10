ee3fddde4676163383e857e53bce77481921245a
Olivier Goulet <olivier_goulet@icloud.com>
2022-03-10 15:56:42 -0500

    Added links to navbar

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -26,9 +26,9 @@
         <ul>
             <li><a href='/#home'>Home</a></li>
             <li><a href='/#about'>About</a></li>
-            <li>Projects</li>
-            <li>Resume</li>
-            <li>Contact</li>
+            <li><a href="/#projects">Projects</a></li>
+            <li><a href="/#resume">Resume</li>
+            <li><a href="/#contact">Contact</a></li>
         </ul>
     </div>
     <div class="right">
