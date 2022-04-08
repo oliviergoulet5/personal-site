91b544e6504cda6078949b48b17fcec5b21e5d83
Olivier Goulet <olivier_goulet@icloud.com>
2022-04-08 01:43:21 -0400

    Add next button

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FilledRoundButton } from '../../components/filledRoundButton/FilledRoundButton';
+import { IconButton } from '../../components/iconButton/IconButton';
 import { RoundButton } from '../../components/roundButton/RoundButton';
+import { IoArrowDown } from 'react-icons/io5';
 
 export function Home() {
     const { t } = useTranslation();
@@ -12,6 +14,7 @@ export function Home() {
                 <span className="absolute h-96 w-full bg-repeat-x bg-contain" style={{ backgroundImage: "url(images/doodle.png)" }} />
             </div>
         <div className="relative flex flex-col h-screen justify-center">
+            <span className="w-full h-72 flex-initial"></span>
             <div className="flex flex-col-reverse md:flex-row justify-center sm:text-left sm:justify-start items-center">
                 <div className="text-center md:text-left">
                     <h1 className='text-2xl md:text-6xl mb-2'>{ t("home.heading") }</h1>
@@ -22,9 +25,12 @@ export function Home() {
                     <img src="images/portrait.png" alt="" className="rounded-full p-1" />
                 </div>
             </div>
-            <div className="flex justify-center space-x-2 my-8">
+            <div className="flex justify-center space-x-2 my-24">
                 <FilledRoundButton text={ t("home.buttons.contact") } />
                 <RoundButton text={ t("home.buttons.download") } />
+            </div>
+            <div className="m-auto">
+                <IconButton icon={ IoArrowDown } /> 
             </div>
         </div>
         </>
