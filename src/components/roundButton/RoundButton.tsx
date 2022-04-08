864a3efb94da40ff900b0777e1508de50a1a2c68
Olivier Goulet <olivier_goulet@icloud.com>
2022-04-08 08:19:35 -0400

    Add social media links

--- src/components/roundButton/RoundButton.tsx
+++ src/components/roundButton/RoundButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 export function RoundButton({ text, onClick: click }: Props) {
 
     return (
-        <div className="group transition duration-500 rounded-full p-1 cursor-pointer border-2 border-transparent hover:border-green-300 select-none">
+        <div className="w-fit group transition duration-500 rounded-full p-1 cursor-pointer border-2 border-transparent hover:border-green-300 select-none">
             <button onClick={ click } className="border-gray-600 rounded-full py-4 px-6 border-2 group-hover:border-transparent">
                 { text }
             </button>
