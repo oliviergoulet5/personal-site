4673ea6cd5c8dc2897fbaf742cd9047812eccb9e
Olivier Goulet <olivier_goulet@icloud.com>
2022-04-08 06:17:11 -0400

    Tweak dest again

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "public", to: "public" }
+                { from: "public", to: "." }
             ]
         })
     ]
