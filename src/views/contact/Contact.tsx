f05a1a71e881b1769892a3e6af02c9b51fc26585
Olivier Goulet <olivier_goulet@icloud.com>
2022-04-09 23:45:36 -0400

    Improve responsive design for contact

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -13,20 +13,20 @@ export function Contact() {
         <div id="contact">
             <h1 className="ps-heading text-center mb-3">Contact</h1>
             <p className="text-gray-300 text-center mb-6">Feel free to send me a message through the site or social media!</p>
-            <div className="flex justify-center mb-8 items-center space-x-4 sm:space-x-8">
+            <div className="flex flex-col sm:flex-row justify-center mb-8 items-center space-y-6 sm:space-y-0 sm:space-x-8">
                 <RoundButton onClick={ () => setActiveSection("form") } text="Message" />
                 <p className="font-bold">
                     OR
                 </p>
                 <div className="flex space-x-3">
                     <button onClick={  () => setActiveSection("discord") } className="transition duration-300 rounded-full p-2 border-2 border-transparent hover:border-white" style={{ backgroundColor: "#5865F2" }}>
-                        <SiDiscord color="#FFF" size="100%" className="w-4 h-4 sm:h-8 sm:w-8" />
+                        <SiDiscord color="#FFF" size="100%" className="w-8 h-8" />
                     </button>
                     <a className="transition duration-300 rounded-full p-2 border-2 border-transparent hover:border-white" href="https://www.linkedin.com/in/olivier-goulet/" style={{ backgroundColor: "#0e76a8" }}>
-                        <ImLinkedin2 color="#FFF" size="100%" className="h-4 w-4 sm:h-8 sm:w-8" />
+                        <ImLinkedin2 color="#FFF" size="100%" className="h-8 w-8" />
                     </a>
                     <a className="transition duration-300 rounded-full p-2 border-2 border-transparent hover:border-white" href="https://github.com/oliviergoulet5" style={{ backgroundColor: "#6e5494" }}>
-                        <DiGithubAlt color="#FFF" size="100%" className="w-4 h-4 sm:h-8 sm:w-8" />
+                        <DiGithubAlt color="#FFF" size="100%" className="w-8 h-8" />
                     </a>
                 </div>
             </div>
@@ -40,11 +40,11 @@ export function Contact() {
 
             { activeSection === "form" && 
                 <form className="w-full sm:w-8/12 m-auto grid grid-cols-2 gap-4">
-                        <div className="ps-input-group">
+                        <div className="ps-input-group col-span-2 sm:col-span-1">
                             <label className="ps-label">Email</label>
                             <input className="ps-input"/>
                         </div>
-                        <div className="ps-input-group">
+                        <div className="ps-input-group col-span-2 sm:col-span-1">
                             <label className="ps-label">Name</label>
                             <input className="ps-input" />
                         </div>
@@ -54,7 +54,7 @@ export function Contact() {
                         </div>
                         <div className="ps-input-group col-span-2">
                             <label className="ps-label">Message</label>
-                            <textarea className="ps-input" />
+                            <textarea className="ps-input" rows={5} />
                         </div>
                 </form>
             }
