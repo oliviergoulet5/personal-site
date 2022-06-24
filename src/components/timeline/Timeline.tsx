94e519530b8722f057e0c399b17d0c2727b36663
Olivier Goulet <olivier_goulet@icloud.com>
2022-06-24 19:09:46 -0400

    Create TimelineEvent component and make events responsive

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { IoBriefcase, IoSchool, IoConstruct, IoHappy } from "react-icons/io5"
-import { EventType, Event } from "../../types";
+import { Event } from "../../types";
+import { TimelineEvent } from "../timelineEvent/TimelineEvent";
 
 interface Props {
     events: Event[];
@@ -8,13 +8,6 @@ interface Props {
     setSelectedEvent: React.Dispatch<number>;
 }
 
-const iconMap: Record<EventType, any> = {
-    education: IoSchool,
-    career: IoBriefcase,
-    job: IoBriefcase,
-    project: IoConstruct,
-    activity: IoHappy
-}
 
 export function Timeline({ events, setSelectedEvent, selectedEvent }: Props) {
 
@@ -23,18 +16,7 @@ export function Timeline({ events, setSelectedEvent, selectedEvent }: Props) {
             <div className="relative flex justify-between">
                 <hr className="absolute w-full my-auto mt-3 border-gray-500" />
                 {
-                    events.map((e, i) => {
-                        const isSelectedEvent = selectedEvent === i;
-                        return <div 
-                            onClick={ () => setSelectedEvent(i) }
-                            className={`cursor-pointer w-8 h-8 p-1 rounded-full border-2 ${ isSelectedEvent ? "border-green-300" : "border-gray-500 hover:border-gray-400" } z-10 bg-gray-800`}>
-                                { React.createElement(iconMap[e.type], { size:"100%", className: isSelectedEvent ? "text-gray-300" : "text-gray-400" }) }
-                                <div className="mt-6 text-center whitespace-nowrap">
-                                    <p className="text-sm">{ e.title }</p>
-                                    <p className="text-xs">{ e.date } </p>
-                                </div>
-                            </div>
-                    })
+                    events.map((e, i) => <TimelineEvent event={e} isSelected={selectedEvent === i} onClick={ () => setSelectedEvent(i) } />) 
                 }
             </div>
         </div>
---
+++ src/components/timelineEvent/TimelineEvent.tsx
@@ -0,0 +1,42 @@
+import { EventType, Event } from "../../types"
+import { IoSchool, IoBriefcase, IoConstruct, IoHappy } from "react-icons/io5";
+import React from "react";
+
+interface Props {
+    event: Event
+    isSelected: boolean;
+    onClick: React.MouseEventHandler
+}
+
+const iconMap: Record<EventType, any> = {
+    education: IoSchool,
+    career: IoBriefcase,
+    job: IoBriefcase,
+    project: IoConstruct,
+    activity: IoHappy
+}
+
+export function TimelineEvent({ event, isSelected, onClick }: Props) {
+    const icon = { 
+        element: iconMap[event.type],
+        props: {
+            size: "100%",
+            className: isSelected ? "text-gray-300" : "text-gray-400"
+        }
+    };
+
+    const circleColor = isSelected ? "border-green-300" : "border-gray-500 hover:border-gray-400";
+
+    return (
+        <div
+            onClick={ onClick }
+            className={`cursor-pointer w-8 h-8 p-1 rounded-full border-2 z-10 bg-gray-800 ${ circleColor }`}
+        >
+            { React.createElement(icon.element, icon.props) }
+            <div className={`mt-6 text-center whitespace-nowrap ${ isSelected || "hidden lg:block"}`}>
+                <p className="text-sm">{ event.title }</p>
+                <p className="text-xs">{ event.date }</p>
+            </div>
+        </div>
+    )
+}
